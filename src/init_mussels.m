03eed9275ffae8e63c38c6339ec07ea6d5a8cafa
anniekovac <anniekovac95@gmail.com>
2016-05-10 11:27:35 +0200

    Add files via upload

--- src/init_mussels.m
+++ src/init_mussels.m
@@ -1,67 +1,57 @@
-
-%number of agents
-%prompt = 'What is the number of agents?';
-%n = input(prompt);
 n=5;
 %adjacency matrix
-Ao = zeros(n,n);
+A_0 = zeros(n,n);
 
-%flag - first time
-flag = 1;
-it = 0;
 pr = 1;
 
 %incijalizacija pozicija
 speed=zeros(3,n);
 
-Xo = zeros(1,n);
-Yo = zeros(1,n);
-Zo = zeros(1,n);
+r_0=1;
+X_0 = zeros(1,n);
+Y_0 = zeros(1,n);
+Z_0 = zeros(1,n);
 
 pos_0=zeros(3,n);
 
 %set_param('model1/Brzina', 'Data', mat2str(speed));
-set_param('model1/Integrator_brzine', 'InitialCondition', mat2str([Xo; Yo; Zo]));
+%set_param('model1/Integrator_brzine', 'InitialCondition', mat2str([Xo; Yo; Zo]));
 
 %stvaranje matrice pozicije
-    pos_0(1,:) = Xo;
-    pos_0(2,:) = Yo;
-    pos_0(3,:) = Zo;
-
-
-while (flag == 1)
-it = it + 1;          %number of iterations
-
-if (n==5)
-    Ao = [0 1 0 0 0 ;
-         1 0 1 0 1 ; 
-         0 1 0 1 1 ; 
-         0 0 1 0 0 ; 
-         0 1 1 0 0];
-else
+    pos_0(1,:) = X_0;
+    pos_0(2,:) = Y_0;
+    pos_0(3,:) = Z_0;
     
-    %napraviti A iz pocetnih pozicija
-    
-    for i = 1 : n
-        for j = 1 : n
-            if (i==j)
-                Ao(i,j) = 0;
-            else
-                Ao(i,j) = randi([0 1]);
-            end
+%napraviti A iz pocetnih pozicija
+    for i = 1:n
+        for j = 1:n
+        %odredivanje udaljenosti izmedju musula - prvo x, y, pa z
+        distance_0 = sqrt((pos_0(1,i)-pos_0(1,j))^2 + (pos_0(2,i)-pos_0(2,j))^2+(pos_0(3,i)-pos_0(3,j))^2);
+        
+        %provjera mogu li komunicirati
+        if distance_0 < r_0
+            
+                %element ne komunicira sam sa sobom
+                if i == j
+                    A_0(i,j) = 0;
+                else
+                    A_0(i,j) = 1;
+                end
+                        
+        else
+                A_0(i,j) = 0;
+        
+        end
         end
     end
-    Ao = double((Ao + Ao') /2 > 0);
-end          
-
-
+    
 %initialization of trust values
 zeta_0 = zeros(n,n);
 for i = 1 : n
     for j = 1 : n
         if i == j
             zeta_0(i,j) = 1;        %initial trust regarding themselves
-        elseif Ao(i,j) == 1
+        elseif A_0(i,j) == 1
             zeta_0(i,j) = 0.1;      %initial trust regarding their neighbours
         end
     end
@@ -74,23 +64,37 @@ K = 0.02;
 sigma_0 = zeros(n,n);
 c = zeros(n,n);
 
+if (n==5) 
+    sigma_0 = [ 0    0.7   0    0    0       
+                1     0    1    0    1
+                0    0.5   0   1.4   1
+                0     0    1    0    0 
+                0    0.4   1    0    0];
+            
+    c = [ 0   0.2   0   0   0 ; 
+         0.8   0   0.4  0   1 ;
+          0   0.2   0  0.6  1 ;
+          0    0   0.4  0   0 ;
+          0   0.2  0.4  0   0 ];
+else
   for i=1:n 
-    delta = round(rand(),-1);
+    delta = roundn(rand(),-1);
     for j=1:n
-        if Ao(j,i) == 1
-            sigma_0(j,i) = round(rand(),-1);    
+        if A(j,i) == 1
+            sigma_0(j,i) = roundn(rand(),-1);    
             c(j,i) = delta;
         end
     end
   end   
+end
 
 
 %adaptation ON/OFF
 adapt = 1;
 
 %check a spanning tree
-deg = diag(sum(Ao,2));
-L = deg - Ao;
+deg = diag(sum(A_0,2));
+L = deg - A_0;
 row_sum = sum (L,2);
 
 %V - full matrix whose columns are the corresponding eigenvectors
@@ -104,8 +108,4 @@ if (number == 1)
     pr = round(L * V(:,find(D==0)));
 end
 
-if (row_sum == 0 & pr == 0 & number == 1) 
-    flag = 0;
-end 
 
-end
