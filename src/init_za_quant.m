86548881633c4437f9b5f1214ac3df0152fec392
anniekovac <anniekovac95@gmail.com>
2016-05-16 22:38:03 +0200

    Add files via upload

---
+++ src/init_za_quant.m
@@ -0,0 +1,135 @@
+n = 5;
+
+%adjacency matrix
+A = zeros(n,n);
+
+it = 0;
+pr = 1;
+
+A_0 = zeros(n,n);
+
+pr = 1;
+
+%incijalizacija pozicija
+speed=zeros(3,n);
+
+r_0=1.5;
+X_0 = zeros(1,n);
+Y_0 = zeros(1,n);
+Z_0 = zeros(1,n);
+
+pos_0=zeros(3,n);
+
+%stvaranje matrice pozicije
+    pos_0(1,:) = X_0;
+    pos_0(2,:) = Y_0;
+    pos_0(3,:) = Z_0;
+    
+if n==5
+    pos_0=[0 0 0 0 1; 
+           4 3 2 1 2.5 ;
+           0 0 0 0 0 ];
+
+    
+%napraviti A iz pocetnih pozicija
+    for i = 1:n
+        for j = 1:n
+        %odredivanje udaljenosti izmedju musula - prvo x, y, pa z
+        distance_0 = sqrt((pos_0(1,i)-pos_0(1,j))^2 + (pos_0(2,i)-pos_0(2,j))^2+(pos_0(3,i)-pos_0(3,j))^2)
+        
+        %provjera mogu li komunicirati
+        if distance_0 < r_0
+            
+                %element ne komunicira sam sa sobom
+                if i == j
+                    A_0(i,j) = 0;
+                else
+                    A_0(i,j) = 1;
+                end
+                        
+        else
+                A_0(i,j) = 0;
+        
+        end
+        end
+    end
+
+
+%initialization of trust values
+zeta_0 = zeros(n,n);
+
+if (n==5)
+    zeta_0 = [ 1   0.1   0   0   0 ; 
+              0.1   1   0.1  0   0.1 ;
+               0   0.1   1  0.1  0.1 ;
+               0    0   0.1  1   0 ;
+               0   0.1  0.1  0   1 ];
+
+else
+    
+    for i = 1 : n
+        for j = 1 : n
+            if i == j
+                zeta_0(i,j) = 1;        %initial trust regarding themselves
+            elseif A(i,j) == 1
+                zeta_0(i,j) = 0.1;      %initial trust regarding their neighbours
+            end
+        end
+    end
+end
+
+%adaptation gain
+K = 0.02;   
+
+%initialization of confidence value and delta value
+sigma_0 = zeros(n,n);
+c = zeros(n,n);
+
+if (n==5) 
+    sigma_0 = [ 0    0.7   0    0    0       
+                1     0    1    0    1
+                0    0.5   0   1.4   1
+                0     0    1    0    0 
+                0    0.4   1    0    0];
+            
+    c = [ 0   0.2   0   0   0 ; 
+         0.8   0   0.4  0   1 ;
+          0   0.2   0  0.6  1 ;
+          0    0   0.4  0   0 ;
+          0   0.2  0.4  0   0 ];
+else
+  for i=1:n 
+    delta = roundn(rand(),-1);
+    for j=1:n
+        if A(j,i) == 1
+            sigma_0(j,i) = roundn(rand(),-1);    
+            c(j,i) = delta;
+        end
+    end
+  end   
+end
+
+%adaptation ON/OFF
+adapt = 1;
+
+%check a spanning tree
+deg = diag(sum(A,2));
+L = deg - A;
+row_sum = sum (L,2);
+
+%V - full matrix whose columns are the corresponding eigenvectors
+%D - diagonal matrix D of eigenvalues
+[V, D] = eig(L);
+D = diag(D);
+D = floor(D);
+
+number = size((find(D == 0)),1);     %number of eigenvalues = 0
+if (number == 1)
+    pr = round(L * V(:,find(D==0)));
+end
+
+if (row_sum == 0 & pr == 0 & number == 1) 
+    flag = 0;
+end 
+
+end
---
+++ src/plot_quant.m
@@ -0,0 +1,251 @@
+close all
+
+
+tmin = 0;
+tmax = 10;
+
+colours = ['r','g','b','m','c','k','k'];
+z1 = [];
+z2 = [];
+z3 = [];
+z4 = [];
+z5 = [];
+
+z1_quant001 = [];
+z2_quant001 = [];
+z3_quant001 = [];
+z4_quant001 = [];
+z5_quant001 = [];
+
+z1_quant005 = [];
+z2_quant005 = [];
+z3_quant005 = [];
+z4_quant005 = [];
+z5_quant005 = [];
+
+z1_quant01 = [];
+z2_quant01 = [];
+z3_quant01 = [];
+z4_quant01 = [];
+z5_quant01 = [];
+
+
+z1_quant025 = [];
+z2_quant025 = [];
+z3_quant025 = [];
+z4_quant025 = [];
+z5_quant025 = [];
+
+
+T = [];
+T_quant001 = [];
+T_quant01 = [];
+T_quant005= [];
+T_quant025= [];
+
+
+for i = 1 : 10 : length(t)
+    z1 = [z1; reshape(zeta(1,:,i), 1, 5)];
+    z2 = [z2; reshape(zeta(2,:,i), 1, 5)];
+    z3 = [z3; reshape(zeta(3,:,i), 1, 5)];
+    z4 = [z4; reshape(zeta(4,:,i), 1, 5)];
+    z5 = [z5; reshape(zeta(5,:,i), 1, 5)];
+    T = [T; t(i)];
+end
+
+
+for i = 1 : 10 : length(t)
+    z1_quant01 = [z1_quant01; reshape(z_quant01(1,:,i), 1, 5)];
+    z2_quant01 = [z2_quant01; reshape(z_quant01(2,:,i), 1, 5)];
+    z3_quant01 = [z3_quant01; reshape(z_quant01(3,:,i), 1, 5)];
+    z4_quant01 = [z4_quant01; reshape(z_quant01(4,:,i), 1, 5)];
+    z5_quant01 = [z5_quant01; reshape(z_quant01(5,:,i), 1, 5)];
+    T_quant01 = [T_quant01; t(i)];
+end
+
+for i = 1 : 10 : length(t)
+    z1_quant025 = [z1_quant025; reshape(z_quant025(1,:,i), 1, 5)];
+    z2_quant025 = [z2_quant025; reshape(z_quant025(2,:,i), 1, 5)];
+    z3_quant025 = [z3_quant025; reshape(z_quant025(3,:,i), 1, 5)];
+    z4_quant025 = [z4_quant025; reshape(z_quant025(4,:,i), 1, 5)];
+    z5_quant025 = [z5_quant025; reshape(z_quant025(5,:,i), 1, 5)];
+    T_quant025 = [T_quant025; t(i)];
+end
+
+%{
+for i = 1 : 10 : length(t)
+    z1_quant001 = [z1_quant001; reshape(z_quant001(1,:,i), 1, 5)];
+    z2_quant001 = [z2_quant001; reshape(z_quant001(2,:,i), 1, 5)];
+    z3_quant001 = [z3_quant001; reshape(z_quant001(3,:,i), 1, 5)];
+    z4_quant001 = [z4_quant001; reshape(z_quant001(4,:,i), 1, 5)];
+    z5_quant001 = [z5_quant001; reshape(z_quant001(5,:,i), 1, 5)];
+    T_quant001 = [T_quant001; t(i)];
+end
+
+
+for i = 1 : 10 : length(t)
+    z1_quant005 = [z1_quant005; reshape(z_quant005(1,:,i), 1, 5)];
+    z2_quant005 = [z2_quant005; reshape(z_quant005(2,:,i), 1, 5)];
+    z3_quant005 = [z3_quant005; reshape(z_quant005(3,:,i), 1, 5)];
+    z4_quant005 = [z4_quant005; reshape(z_quant005(4,:,i), 1, 5)];
+    z5_quant005 = [z5_quant005; reshape(z_quant005(5,:,i), 1, 5)];
+    T_quant005 = [T_quant005; t(i)];
+end
+%}
+
+%{
+%% PLOTANJE KONTINUIRANOG I DISKRETIZIRANOG S 0.1
+figure
+plot(T, [z1(:,1), z2(:,1), z3(:,1), z4(:,1), z5(:,1)], T_quant01, [z1_quant01(:,1), z2_quant01(:,1), z3_quant01(:,1), z4_quant01(:,1), z5_quant01(:,1)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 1')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+%%
+
+figure
+plot(T, [z1(:,2), z2(:,2), z3(:,2), z4(:,2), z5(:,2)], T_quant01, [z1_quant01(:,2), z2_quant01(:,2), z3_quant01(:,2), z4_quant01(:,2), z5_quant01(:,2)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 2')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,3), z2(:,3), z3(:,3), z4(:,3), z5(:,3)], T_quant01, [z1_quant01(:,3), z2_quant01(:,3), z3_quant01(:,3), z4_quant01(:,3), z5_quant01(:,3)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 3')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,4), z2(:,4), z3(:,4), z4(:,4), z5(:,4)], T_quant01, [z1_quant01(:,4), z2_quant01(:,4), z3_quant01(:,4), z4_quant01(:,4), z5_quant01(:,4)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 4')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,5), z2(:,5), z3(:,5), z4(:,5), z5(:,5)], T_quant01, [z1_quant01(:,5), z2_quant01(:,5), z3_quant01(:,5), z4_quant01(:,5), z5_quant01(:,5)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 5')
+%legend('kontinuirano', 'diskretno s 0.1', 'diskretno s 0.01')
+axis([tmin tmax 0 1])
+%}
+
+
+%% PLOTANJE KONTINUIRANOG I DISKRETIZIRANOG S 0.25
+figure
+plot(T, [z1(:,1), z2(:,1), z3(:,1), z4(:,1), z5(:,1)], T_quant025, [z1_quant025(:,1), z2_quant025(:,1), z3_quant025(:,1), z4_quant025(:,1), z5_quant025(:,1)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 1')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+%%
+
+figure
+plot(T, [z1(:,2), z2(:,2), z3(:,2), z4(:,2), z5(:,2)], T_quant025, [z1_quant025(:,2), z2_quant025(:,2), z3_quant025(:,2), z4_quant025(:,2), z5_quant025(:,2)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 2')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,3), z2(:,3), z3(:,3), z4(:,3), z5(:,3)], T_quant025, [z1_quant025(:,3), z2_quant025(:,3), z3_quant025(:,3), z4_quant025(:,3), z5_quant025(:,3)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 3')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,4), z2(:,4), z3(:,4), z4(:,4), z5(:,4)], T_quant025, [z1_quant025(:,4), z2_quant025(:,4), z3_quant025(:,4), z4_quant025(:,4), z5_quant025(:,4)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 4')
+%legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,5), z2(:,5), z3(:,5), z4(:,5), z5(:,5)], T_quant025, [z1_quant025(:,5), z2_quant025(:,5), z3_quant025(:,5), z4_quant025(:,5), z5_quant025(:,5)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 5')
+%legend('kontinuirano', 'diskretno s 0.1', 'diskretno s 0.01')
+axis([tmin tmax 0 1])
+%}
+
+%{
+%%
+figure
+plot(T, [z1(:,1), z2(:,1), z3(:,1), z4(:,1), z5(:,1)], T_quant01, [z1_quant01(:,1), z2_quant01(:,1), z3_quant01(:,1), z4_quant01(:,1), z5_quant01(:,1)],T_quant001, [z1_quant001(:,1), z2_quant001(:,1), z3_quant001(:,1), z4_quant001(:,1), z5_quant001(:,1)],T_quant005, [z1_quant005(:,1), z2_quant005(:,1), z3_quant005(:,1), z4_quant005(:,1), z5_quant005(:,1)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 1')
+legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+%%
+
+figure
+plot(T, [z1(:,2), z2(:,2), z3(:,2), z4(:,2), z5(:,2)], T_quant01, [z1_quant01(:,2), z2_quant01(:,2), z3_quant01(:,2), z4_quant01(:,2), z5_quant01(:,2)],T_quant001, [z1_quant001(:,2), z2_quant001(:,2), z3_quant001(:,2), z4_quant001(:,2), z5_quant001(:,2)],T_quant005, [z1_quant005(:,2), z2_quant005(:,2), z3_quant005(:,2), z4_quant005(:,2), z5_quant005(:,2)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 2')
+legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,3), z2(:,3), z3(:,3), z4(:,3), z5(:,3)], T_quant01, [z1_quant01(:,3), z2_quant01(:,3), z3_quant01(:,3), z4_quant01(:,3), z5_quant01(:,3)],T_quant001, [z1_quant001(:,3), z2_quant001(:,3), z3_quant001(:,3), z4_quant001(:,3), z5_quant001(:,3)],T_quant005, [z1_quant005(:,3), z2_quant005(:,3), z3_quant005(:,3), z4_quant005(:,3), z5_quant005(:,3)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 3')
+legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+%%
+figure
+plot(T, [z1(:,4), z2(:,4), z3(:,4), z4(:,4), z5(:,4)], T_quant01, [z1_quant01(:,4), z2_quant01(:,4), z3_quant01(:,4), z4_quant01(:,4), z5_quant01(:,4)],T_quant001, [z1_quant001(:,4), z2_quant001(:,4), z3_quant001(:,4), z4_quant001(:,4), z5_quant001(:,4)],T_quant005, [z1_quant005(:,4), z2_quant005(:,4), z3_quant005(:,4), z4_quant005(:,4), z5_quant005(:,4)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 4')
+legend('kontinuirano', 'diskretno s 0.01', 'diskretno s 0.1', 'diskretno s 0.05')
+axis([tmin tmax 0 1])
+
+
+%%
+figure
+plot(T, [z1(:,5), z2(:,5), z3(:,5), z4(:,5), z5(:,5)], T_quant01, [z1_quant01(:,5), z2_quant01(:,5), z3_quant01(:,5), z4_quant01(:,5), z5_quant01(:,5)],T_quant001, [z1_quant001(:,5), z2_quant001(:,5), z3_quant001(:,5), z4_quant001(:,5), z5_quant001(:,5)],T_quant005, [z1_quant005(:,5), z2_quant005(:,5), z3_quant005(:,5), z4_quant005(:,5), z5_quant005(:,5)]);
+grid on 
+xlabel('t(s)')
+ylabel('povjerenje')
+title('Povjerenje prema agentu 5')
+legend('kontinuirano', 'diskretno s 0.1', 'diskretno s 0.01')
+axis([tmin tmax 0 1])
+%}
