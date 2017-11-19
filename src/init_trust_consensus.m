109380e3eccf2d8fc38b877a12cf3cde0f4daa5b
Annie Kovač <anniekovac95@gmail.com>
2017-11-19 20:04:24 +0100

    Implemented calculating trust based on observation function - doesn't work yet

---
+++ src/init_trust_consensus.m
@@ -0,0 +1,40 @@
+clear all;
+
+% number of agents
+n = 4;
+
+% change A for different topology
+A = ones(n) - eye(n);
+% communication period for scheduled communication
+Td = 5;
+% algorithm step
+eps = 0.1;
+% initial value of x
+x0 = [0 30 50 100];
+% comm type (1=ideal comm, 2=sequenced comm)
+comm_type=1;
+% event based flag -- calculate only on message sent/received
+event_based=1;
+
+% initialization of random sigma matrix
+sigma0 = zeros(n,n);
+for i=1:n 
+    for j=1:n
+        if A(j,i) == 1
+            sigma0(j,i) = roundn(rand(),-1);    
+        end
+    end
+end
+  
+%initialization of trust values
+zeta0 = zeros(n, n);
+for i = 1 : n
+    for j = 1 : n
+        if i == j
+            zeta0(i,j) = 1;        %initial trust regarding themselves
+        elseif A(i,j) == 1
+            zeta0(i,j) = 0.1;      %initial trust regarding their neighbours
+        end
+    end
+end
+
