6279d77380e9a33d6dac040ee7443f0ac5e08d31
Annie Kovač <anniekovac95@gmail.com>
2018-01-10 14:19:54 +0100

    Small bug fixed, but not fully functional yet

--- src/init_trust_consensus.m
+++ src/init_trust_consensus.m
@@ -10,14 +10,14 @@ Td = 5;
 % algorithm step
 eps = 0.1;
 % initial value of x
-x0 = [0 30 50 100];
+x0 = [30 30 30 100];
 % comm type (1=ideal comm, 2=sequenced comm)
 comm_type=2;
 % event based flag -- calculate only on message sent/received
 event_based=1;
 
 % initialization of random sigma matrix
-sigma0 = zeros(n,n);
+sigma0 = ones(n,n);
 for i=1:n 
     for j=1:n
         if A(j,i) == 1
